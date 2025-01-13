84d505aadb1392f26d0d408566b7eae7e2ec492d
123-anubhav <anubhav.aa2@gmail.com>
2025-01-13 23:55:15 +0530

    api gateway added

--- 01_products_ms/src/main/java/in/experience/rest/ProductRestController.java
+++ 01_products_ms/src/main/java/in/experience/rest/ProductRestController.java
@@ -3,27 +3,40 @@ package in.experience.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import in.experience.dto.ProductDTO;
 
 @org.springframework.web.bind.annotation.RestController
+@RequestMapping(value = "/product-api")
 public class ProductRestController {
 
-	@GetMapping("/products")	
-	 public List<ProductDTO> getAllProducts() {
-	        // Here you would typically retrieve the products from a database
-	        // For this example, we'll create a static list of products
+	@Autowired
+	private Environment environment;
 
-		System.out.println("============================       ProductRestController.getAllProducts() EXECUTES ============================");
-		
-	        List<ProductDTO> products = new ArrayList<>();
+	@GetMapping("/products")
+	public List<ProductDTO> getAllProducts() {
+		// Here you would typically retrieve the products from a database
+		// For this example, we'll create a static list of products
+		 String port = environment.getProperty("server.port");
 
-	        // Adding some sample products
-	        products.add(new ProductDTO(1L, "Product 1", "Description of Product 1", 99.99, "Electronics"));
-	        products.add(new ProductDTO(2L, "Product 2", "Description of Product 2", 49.99, "Furniture"));
-	        products.add(new ProductDTO(3L, "Product 3", "Description of Product 3", 19.99, "Clothing"));
+	        // Print the server.port value to the console
+	        System.out.println("============================       Server is running on port: " + port + " ============================");
 
-	        return products;
-	    }
+	        
+		System.out.println(
+				"============================       ProductRestController.getAllProducts() EXECUTES ============================");
+
+		List<ProductDTO> products = new ArrayList<>();
+
+		// Adding some sample products
+		products.add(new ProductDTO(1L, "Product 1", "Description of Product 1", 99.99, "Electronics"));
+		products.add(new ProductDTO(2L, "Product 2", "Description of Product 2", 49.99, "Furniture"));
+		products.add(new ProductDTO(3L, "Product 3", "Description of Product 3", 19.99, "Clothing"));
+
+		return products;
+	}
 }
